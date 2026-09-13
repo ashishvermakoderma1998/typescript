3a751a82f3478ce8d67da5d4f1d13c66bc2ab2fb
Ashish <ashishvermakoderma@gmail.com>
2026-09-13 07:04:32 +0530

    update

--- array.ts
+++ array.ts
@@ -6,6 +6,6 @@ students.push("Sangita");
 console.log(students);
 
 var collegeName:ReadonlyArray<string>=["J J college","Commerce College", "Basukala ITI"];
-// collegeName.push("markham college");
+// collegeName.push("markha college");
 console.log(collegeName);
 
